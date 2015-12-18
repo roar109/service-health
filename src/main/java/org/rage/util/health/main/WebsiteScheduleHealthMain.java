1a500babcab3afbc7f9665fe433fef4a8174ed41
Hector Mendoza <hmendoza@24hourfit.com>
2015-12-18 17:08:49 -0600

    Simple refactor to the scheduler class

--- src/main/java/org/rage/util/health/main/WebsiteScheduleHealthMain.java
+++ src/main/java/org/rage/util/health/main/WebsiteScheduleHealthMain.java
@@ -1,14 +1,12 @@
 package org.rage.util.health.main;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-
-import org.rage.util.service.health.impl.WebsiteHealthServiceImpl;
-
-import static java.util.concurrent.TimeUnit.*;
+import static java.util.concurrent.TimeUnit.MINUTES;
 
 import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.rage.util.service.health.impl.WebsiteHealthServiceImpl;
 
 public class WebsiteScheduleHealthMain {
 	private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
@@ -16,13 +14,7 @@ public class WebsiteScheduleHealthMain {
 	public static void main(String[] args) {
 		System.out.println("Starting: "+new Date());
 		Runnable healthCheckJob = () -> { System.out.println(new Date()); new WebsiteHealthServiceImpl().checkHealthStatus(args); };
-		final ScheduledFuture<?> beeperHandle =  scheduler.scheduleAtFixedRate(healthCheckJob, 1, 5, MINUTES);
-		try {
-			System.out.println("-- "+new Date());
-			Thread.sleep(1000*60);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		scheduler.scheduleAtFixedRate(healthCheckJob, 1, 5, MINUTES);
 	}
 
 }
